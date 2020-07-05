c7a56023a8edbea0df62e45991ed88846d6ae959
Muhammedojo <Ojomuhammad@gmail.com>
2020-07-05 10:35:23 +0100

    finishing nav inside dev branch

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import '../App.css'
 function Nav() {
     return (
         <div>
             <header className="header-section">
-                <div className="container-fluid">
+                <div className="container">
+                    <div className="row" style={{ background: "black" }}>
+
+                        <a href="#" className="site-btn m-3">Home</a>
+                        <a href="#" className="site-btn m-3">About Us</a>
+                        <a href="#" className="site-btn m-3">Services</a>
+                        <a href="#" className="site-btn m-3">Contact Us</a>
+                        <a href="#" className="site-btn m-3">Clients</a>
+                        <a href="#" className="site-btn m-3">Portfolio</a>
+
+                    </div>
 
                 </div>
 
