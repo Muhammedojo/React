11c416888c97af3253774b1f80a663797188b311
Muhammedojo <Ojomuhammad@gmail.com>
2020-07-05 12:04:20 +0100

    Adding img to the home page

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -5,14 +5,14 @@ function Nav() {
         <div>
             <header className="header-section">
                 <div className="container">
-                    <div className="row" style={{background:"black"}}>
+                    <div className="row" style={{ background: "black", color: "white"}}>
                        
-                                <a href="#" className="site-btn m-3">Home</a>
-                                <a href="#" className="site-btn m-3">About Us</a>
-                                <a href="#" className="site-btn m-3">Services</a>
-                                <a href="#" className="site-btn m-3">Contact Us</a>
-                                <a href="#" className="site-btn m-3">Clients</a>
-                                <a href="#" className="site-btn m-3">Portfolio</a>
+                        <a href="#" className="site-btn m-3" style={{ background: "black", color: "white" }}>Home</a>
+                        <a href="#" className="site-btn m-3" style={{ background: "black", color: "white" }}>About Us</a>
+                        <a href="#" className="site-btn m-3" style={{ background: "black", color: "white" }}>Services</a>
+                        <a href="#" className="site-btn m-3" style={{ background: "black", color: "white" }}>Contact Us</a>
+                        <a href="#" className="site-btn m-3" style={{ background: "black", color: "white" }}>Clients</a>
+                        <a href="#" className="site-btn m-3" style={{ background: "black", color: "white" }}>Portfolio</a>
                         
                         </div>
 
--- src/component/home.jsx
+++ src/component/home.jsx
@@ -1,31 +1,16 @@
-import React, {Fragment} from 'react'
-import 'bootstrap/dist/css/bootstrap.css'
+import React, {Fragment} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import job7 from '../job7.jpg';
 function Home() {
     
     return (
         <Fragment>
         <div>
             <h1><i>Welcome!!!</i></h1>
-            <span>How can we be of service </span>
         </div>
-        <div className="social-section">
-            <div className="container">
-                <div className="row">
-                    <div className="col-xl-10 offset-xl-1">
-                        <div className="social-link-warp">
-                            <div className="social-links" style={{backgroun:"blue"}}>
-                                <a href=""><i className="fa fa-pinterest"></i></a>
-                                <a href=""><i className="fa fa-linkedin"></i></a>
-                                <a href=""><i className="fa fa-instagram"></i></a>
-                                <a href=""><i className="fa fa-facebook"></i></a>
-                                <a href=""><i className="fa fa-twitter"></i></a>
-                            </div>
-                            <h2 className="hidden-md hidden-sm">My Social Profiles</h2>
-                        </div>
-                    </div>
-                </div>
+            <div className="container-fluid">
+                <img src={job7} alt="image" style={{ width: "100%", height: "500px" }}/>
             </div>
-        </div>
         </Fragment>	
     );
 }
