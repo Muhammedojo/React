e32cf51c97cfda5f1d68f7259870b66bc4a3ecb7
Muhammedojo <Ojomuhammad@gmail.com>
2020-07-05 10:14:03 +0100

    resolving merge conflict

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
 import React, { Component } from 'react'
 import './App.css';
 import Home from './component/home'
 import Nav from './component/Nav'
-
-function App() {
-  return (
-     <div className="container">
-      <Nav />
-      <Home/>
-      </div>
-=======
-import React from 'react';
-import './App.css';
 import Footer from './component/Footer';
 
 function App() {
   return (
-    <div className="container">
-   <Footer/>
->>>>>>> dev
-    </div>
-    
+     <div className="container">
+        <Nav />
+        <Home/>
+        <Footer />
+      </div>   
   );
 }
 
