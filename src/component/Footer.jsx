d7497f9c0e7eaa68013d9351483eacfac2d61ee0
Muhammedojo <Ojomuhammad@gmail.com>
2020-07-05 09:59:09 +0100

    testing footer in dev

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,10 +4,15 @@ function Footer() {
     return (
         <footer className="footer-section">
             <div className="container text-center">
-                <div className="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with
+                <div className="copyright">
+                    <div> 
+                        Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0.
+                    </div>
+        <div>
+         Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with
 <i className="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://boldlinks.com.ng" target="_blank">Boldlinks</a>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </div>
+                   
 </div>
             </div>
         </footer>
